6f16823ff761d32e6b0824bfbc8363407e307aae
CapColor <capitancolorph4@gmail.com>
2023-07-01 02:58:55 -0300

    zxzc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import { defineConfig } from 'astro/config'
+
 export default defineConfig({
+  site: 'https://lasnacionesunidasdeamerica.github.io',
+  base: '/lasnacionesunidasdeamerica.github.io',
   integrations: [tailwind()]
-});
+})
+
